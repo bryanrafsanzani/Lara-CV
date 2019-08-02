700e1111c611a28fc1df94772c0f88b3f32e9295
bryanrafsanzani <bryanrafsanzani46@gmail.com>
2019-08-02 21:54:47 +0700

    update: commit apa

---
+++ resources/views/dashboard/hobby-all.blade.php
@@ -0,0 +1,15 @@
+@extends('_layout.dashboard._app')
+    @section('title')
+        Manage Hobby
+    @endsection
+
+    <!-- Body Start -->
+    @section('side-title')
+        <i class="fa fa-soccer-ball-o"></i> <span>Manage Hobby List</span>
+    @endsection
+
+    @section('content')
+        @include('_layout.dashboard._content-sample')
+    @endsection
+    
+    <!-- Body End -->
